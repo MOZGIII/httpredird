3f214dac07e63b072a03cec349ad72d9dce86181
MOZGIII <mike-n@narod.ru>
2015-02-03 15:16:41 +0300

    Using command line args insted of config files

--- httpredird.go
+++ httpredird.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"log"
 	"net/http"
@@ -15,48 +14,16 @@ type ConfigStruct struct {
 	RedirectCode int    `json:"redirect_code"`
 }
 
-// LoadConfig loads config from json file
-func LoadConfig(filename string) (*ConfigStruct, error) {
-	reader, err := os.Open(filename)
-	if err != nil {
-		return nil, err
-	}
-	defer reader.Close()
-
-	decoder := json.NewDecoder(reader)
-
-	config := new(ConfigStruct)
-	err = decoder.Decode(config)
-	if err != nil {
-		return nil, err
-	}
-
-	return config, nil
-}
-
-// MustLoadConfig calls LoadConfig and makes it a must-success
-func MustLoadConfig(filename string) (config *ConfigStruct) {
-	config, err := LoadConfig(filename)
-	if err != nil {
-		panic(err)
-	}
-	return
-}
-
-var configFile string
+var config ConfigStruct
 
 func init() {
-	const (
-		configFileDefault = "config.json"
-		configFileUsage   = "config file to use"
-	)
-	flag.StringVar(&configFile, "config", configFileDefault, configFileUsage)
-	flag.StringVar(&configFile, "c", configFileDefault, configFileUsage+" (shorthand)")
+	flag.StringVar(&(config.TargetURL), "target", "http://127.0.0.1:1234/", "url to reditrect to")
+	flag.StringVar(&(config.BindAddr), "bind", ":80", "address to bind to")
+	flag.IntVar(&(config.RedirectCode), "code", 301, "redirect with this code")
 }
 
 func main() {
 	flag.Parse()
-	config := MustLoadConfig(configFile)
 	err := http.ListenAndServe(config.BindAddr, http.RedirectHandler(config.TargetURL, config.RedirectCode))
 	log.Println(err)
 	log.Fatal("Try setting CAP_NET_BIND_SERVICE: setcap 'cap_net_bind_service=+ep' " + os.Args[0])
